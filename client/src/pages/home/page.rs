14ee8e3a1541f06bfacf39cf7cdad58aeebd07fc
rapha <raphael.thurnherr1990@gmail.com>
2023-05-27 12:24:54 +0200

    add launch button interaction

--- client/src/pages/home/page.rs
+++ client/src/pages/home/page.rs
@@ -1,11 +1,13 @@
 use sycamore::prelude::*;
+use crate::AppRoutes;
 use crate::config::keys;
+use crate::utils::url::safe_navigate;
 
 #[component]
 pub fn SectionOne<G: Html>(cx: Scope) -> View<G> {
     view! {cx,
         div(class="flex justify-center flex-auto flex-shrink-0 min-h-screen") {
-            div(class="flex flex-col justify-between lg:max-w-screen-lg xl:max-w-screen-xl h-full w-full") {
+            div(class="flex flex-col justify-between 2xl:max-w-screen-2xl h-full w-full") {
                 div(class="flex") {
                     div(class="flex flex-col justify-between w-1/3 min-h-screen") {
                         img(class="h-auto w-64 mt-8", src=keys::LOGO_WITH_TEXT_IMG) {}
@@ -16,7 +18,8 @@ pub fn SectionOne<G: Html>(cx: Scope) -> View<G> {
                             p(class="text-2xl my-8") {
                                 "Stay updated on token transfers, new governance proposals, validator problems and ending of unbonding periods that are important for you."
                             }
-                            button(class="rounded-xl flex items-center justify-center px-4 py-2 w-64 text-black hover:text-white bg-gradient-to-r from-primary_gradient-from to-primary_gradient-to") {
+                            button(class="rounded-xl flex items-center justify-center px-4 py-2 w-64 text-black hover:text-white bg-gradient-to-r from-primary_gradient-from to-primary_gradient-to",
+                                    on:click=move |_| safe_navigate(cx, AppRoutes::Login)) {
                                 span(class="text-xl") {
                                     "Launch App"
                                 }
