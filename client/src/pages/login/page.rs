65f5b616c825f06e55d2de75082370dce69fbedd
rapha <raphael.thurnherr1990@gmail.com>
2023-06-19 16:45:00 +0200

    improve login UI

--- client/src/pages/login/page.rs
+++ client/src/pages/login/page.rs
@@ -8,6 +8,92 @@ use crate::components::messages::{create_error_msg_from_status, create_message};
 use crate::config::keys;
 use crate::{AppState, AuthState, InfoLevel, Services};
 
+
+#[component]
+fn KeplrSvg<G: Html>(cx: Scope) -> View<G> {
+    view! (cx,
+        svg(
+            width="24",
+            height="24",
+            viewBox="0 0 42 42",
+            fill="none",
+            xmlns="http://www.w3.org/2000/svg",
+        ) {
+            g(clip-path="url(#clip0_425_5107)") {
+                path(
+                    d="M32.4545 0H9.54545C4.27365 0 0 4.27365 0 9.54545V32.4545C0 37.7264 4.27365 42 9.54545 42H32.4545C37.7264 42 42 37.7264 42 32.4545V9.54545C42 4.27365 37.7264 0 32.4545 0Z",
+                    fill="url(#paint0_linear_425_5107)",
+                )
+                path(
+                    d="M32.4545 0H9.54545C4.27365 0 0 4.27365 0 9.54545V32.4545C0 37.7264 4.27365 42 9.54545 42H32.4545C37.7264 42 42 37.7264 42 32.4545V9.54545C42 4.27365 37.7264 0 32.4545 0Z",
+                    fill="url(#paint1_radial_425_5107)",
+                )
+                path(
+                    d="M32.4545 0H9.54545C4.27365 0 0 4.27365 0 9.54545V32.4545C0 37.7264 4.27365 42 9.54545 42H32.4545C37.7264 42 42 37.7264 42 32.4545V9.54545C42 4.27365 37.7264 0 32.4545 0Z",
+                    fill="url(#paint2_radial_425_5107)",
+                )
+                path(
+                    d="M32.4545 0H9.54545C4.27365 0 0 4.27365 0 9.54545V32.4545C0 37.7264 4.27365 42 9.54545 42H32.4545C37.7264 42 42 37.7264 42 32.4545V9.54545C42 4.27365 37.7264 0 32.4545 0Z",
+                    fill="url(#paint3_radial_425_5107)",
+                )
+                path(
+                    d="M17.2526 32.2614V22.5192L26.7185 32.2614H31.9849V32.0079L21.0964 20.9122L31.1469 10.3857V10.2614H25.8464L17.2526 19.5635V10.2614H12.9849V32.2614H17.2526Z",
+                    fill="white",
+                )
+            }
+            defs {
+                linearGradient(
+                    id="paint0_linear_425_5107",
+                    x1="21",
+                    y1="0",
+                    x2="21",
+                    y2="42",
+                    gradientUnits="userSpaceOnUse",
+                ) {
+                    stop(stop-color="#1FD1FF")
+                    stop(offset="1", stop-color="#1BB8FF")
+                }
+                radialGradient(
+                    id="paint1_radial_425_5107",
+                    cx="0",
+                    cy="0",
+                    r="1",
+                    gradientUnits="userSpaceOnUse",
+                    gradientTransform="translate(2.00623 40.4086) rotate(-45.1556) scale(67.3547 68.3624)",
+                ) {
+                    stop(stop-color="#232DE3")
+                    stop(offset="1", stop-color="#232DE3", stop-opacity="0")
+                }
+                radialGradient(
+                    id="paint2_radial_425_5107",
+                    cx="0",
+                    cy="0",
+                    r="1",
+                    gradientUnits="userSpaceOnUse",
+                    gradientTransform="translate(39.7379 41.7602) rotate(-138.45) scale(42.1137 64.2116)",
+                ) {
+                    stop(stop-color="#8B4DFF")
+                    stop(offset="1", stop-color="#8B4DFF", stop-opacity="0")
+                }
+                radialGradient(
+                    id="paint3_radial_425_5107",
+                    cx="0",
+                    cy="0",
+                    r="1",
+                    gradientUnits="userSpaceOnUse",
+                    gradientTransform="translate(20.6501 0.311498) rotate(90) scale(33.1135 80.3423)",
+                ) {
+                    stop(stop-color="#24D5FF")
+                    stop(offset="1", stop-color="#1BB8FF", stop-opacity="0")
+                }
+                clipPath(id="clip0_425_5107") {
+                    rect(width="42", height="42", fill="white")
+                }
+            }
+        }
+    )
+}
+
 #[derive(Serialize, Deserialize)]
 pub struct JsResult {
     pub result: String,
@@ -60,7 +146,7 @@ fn is_mobile() -> bool {
 #[component]
 pub async fn Login<G: Html>(cx: Scope<'_>) -> View<G> {
     let app_state = use_context::<AppState>(cx);
-    let class_button = "w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm \
+    let class_button = "w-full flex items-center justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm \
     font-medium text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500";
 
     let is_mobile = is_mobile();
@@ -86,37 +172,22 @@ pub async fn Login<G: Html>(cx: Scope<'_>) -> View<G> {
                     path(fill="none", stroke=color, stroke-miterlimit="10", stroke-width="10", d="M284.89,130.92A143.54,143.54,0,0,1,425.43,272.21")
                 }
                 h2(class="mt-6 text-center text-3xl font-extrabold text-gray-900 dark:text-white") {
-                    "Login with Keplr"
+                    "Login to Star Scope"
                 }
             }
             div(class="mt-8 sm:mx-auto sm:w-full sm:max-w-md") {
                 div(class="bg-white dark:bg-purple-700 py-8 px-4 shadow sm:rounded-lg sm:px-10") {
-                    div(class="flex items-center justify-center space-y-6") {
+                    div(class="flex items-center justify-center w-[238px] space-y-6") {
                         button(on:click=move |_| {
-                            spawn_local_scoped(cx, async move {
-                                if *app_state.auth_state.get() == AuthState::LoggedOut {
-                                    match keplr_login_wrapper().await {
-                                        Ok(result) => {
-                                            let response = use_context::<Services>(cx).auth_manager.clone().login(result.clone()).await;
-                                            match response {
-                                                Ok(_) => {
-                                                    let mut auth_state = use_context::<AppState>(cx).auth_state.modify();
-                                                    *auth_state = AuthState::LoggedIn;
-                                                }
-                                                Err(status) => create_error_msg_from_status(cx, status),
-                                            }
-                                        }
-                                        Err(status) => create_message(cx, "Login failed", format!("Login failed with status: {}", status), InfoLevel::Error),
-                                    }
-                                };
-                            });
                         }, class=format!("bg-keplr-blue-500 hover:bg-keplr-blue-600 {} {}", class_button, if is_mobile { "hidden" } else { "" })) {
+                            span(class="mr-2") {KeplrSvg{}}
                             "Keplr Login"
                         }
                         p(class=format!("bg-keplr-blue-500 hover:bg-keplr-blue-600 {}", if is_mobile { "" } else {"hidden"})) { "Mobile devices are not supported yet" }
                     }
-                    div(class="flex items-center justify-center space-y-6 mt-4") {
+                    div(class="flex items-center justify-center w-[238px] space-y-6 mt-4") {
                         a(class=format!("bg-discord-purple-500 hover:bg-discord-purple-600 {}", class_button), href=discord_login_url) {
+                            span(class="w-6 h-6 mr-2 icon-[mingcute--discord-fill]") {}
                             "Discord Login"
                         }
                     }
