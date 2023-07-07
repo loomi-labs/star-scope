1e4d63c6e21f84b380031ece749b17ece172d5a5
rapha <raphael.thurnherr1990@gmail.com>
2023-07-07 15:53:53 +0200

    remove wallet login if on mobile

--- client/src/pages/login/page.rs
+++ client/src/pages/login/page.rs
@@ -1,11 +1,25 @@
-use sycamore::prelude::*;
+use sycamore::{prelude::*, view};
+use wasm_bindgen::prelude::*;
 
 use crate::components::social_media::{CosmosLoginButton, DiscordLoginButton, TelegramLoginButton};
 use crate::config::keys;
 
+#[wasm_bindgen(module = "/src/pages/login/wallet_connect_login.js")]
+extern "C" {
+    fn isMobile(url: String) -> JsValue;
+}
+
+fn is_mobile() -> bool {
+    let result = isMobile(keys::WEB_APP_URL.to_string());
+    serde_wasm_bindgen::from_value(result).unwrap_or(false)
+}
+
 #[component]
 pub async fn Login<G: Html>(cx: Scope<'_>) -> View<G> {
     let color = keys::WHITE_COLOR;
+
+    let is_mobile = is_mobile();
+
     view!(cx,
         div(class="h-screen w-screen bg-gray-100 dark:bg-d-bg flex flex-col justify-center py-12 sm:px-6 lg:px-8") {
             div(class="sm:mx-auto sm:w-full sm:max-w-md flex flex-col justify-center items-center") {
@@ -31,9 +45,17 @@ pub async fn Login<G: Html>(cx: Scope<'_>) -> View<G> {
                     div(class="flex items-center justify-center space-y-6 mt-6") {
                         TelegramLoginButton(web_app_url=keys::WEB_APP_URL.to_string())
                     }
-                    div(class="flex justify-center space-y-6 mt-6") {
-                        CosmosLoginButton()
-                    }
+                    (if !is_mobile {
+                        view!(cx,
+                            div(class="flex justify-center space-y-6 mt-6") {
+                                CosmosLoginButton()
+                            }
+                        )
+                    } else {
+                        view!(cx,
+                            div()
+                        )
+                    })
                 }
             }
         }
